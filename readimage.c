7888e8dbdf057a69cb5b3af9a6314aa990f2b9b5
Dongshi Zhao <donny.zhao@hotmail.com>
2020-11-24 15:17:41 -0500

    Update readimage.c

--- readimage.c
+++ readimage.c
@@ -9,6 +9,20 @@
 
 unsigned char *disk;
 
+char get_inode_type(struct ext2_inode *inode_table, int index) {
+    char file_type;
+    if (inode_table[index].i_mode & EXT2_S_IFLNK) {
+        file_type = '0';
+    } 
+    if (inode_table[index].i_mode & EXT2_S_IFREG) {
+        file_type = 'f';
+    } else {
+        file_type = 'd';
+    }
+
+    return file_type;
+}
+
 void print_bitmap(char *bits, unsigned int len) {
     // print out the char (1 byte) one by one
     for (int i = 0; i < len; ++i){
@@ -47,6 +61,8 @@ int main(int argc, char **argv) {
     printf("    free inodes: %d\n", db->bg_free_inodes_count);
     printf("    used_dirs: %d\n", db->bg_used_dirs_count);
 
+    // BITMAPS --------------------------------------------------------------
+
     unsigned char *block_bitmap = (unsigned char *) (disk + EXT2_BLOCK_SIZE*3);
     printf("Block bitmap: ");
     print_bitmap(block_bitmap, sb->s_blocks_count / 8);
@@ -54,6 +70,8 @@ int main(int argc, char **argv) {
     printf("Inode bitmap: ");
     print_bitmap(inode_bitmap, sb->s_inodes_count / 8);
     printf("\n");
+
+    // BITMAPS --------------------------------------------------------------
 
     struct ext2_inode *inode_table = (struct ext2_inode*) (disk + EXT2_BLOCK_SIZE*db->bg_inode_table);
 
@@ -67,26 +85,81 @@ int main(int argc, char **argv) {
     }
     printf("\n");
 
-
-    unsigned int occupied_count = sb->s_inodes_count - sb->s_free_inodes_count;
+    // print all other inodes in use
     char file_type;
-    for (int i = EXT2_GOOD_OLD_FIRST_INO; i < occupied_count; ++i) {
-        if (inode_table[i].i_mode & S_IFLNK) {
-            file_type = '?'; // Char for links??? TODO
-        } else if (inode_table[i].i_mode & EXT2_S_IFREG) {
-            file_type = 'f';
-        } else {
-            file_type = 'd';
-        }
+    for (int i = EXT2_GOOD_OLD_FIRST_INO; i < sb->s_inodes_count; ++i) {
+        // only if inode is in use
+        if (inode_bitmap[i/8] & (1 << i % 8)) {
+            char file_type = get_inode_type(inode_table, i);
 
-        printf("[%d] type: %c size: %d links: %d blocks %d\n", i + 1, file_type, inode_table[i].i_size, inode_table[i].i_links_count, inode_table[i].i_blocks);
-        printf("[%d] Blocks:", i + 1);
-        for (int j = 0; j < inode_table[i].i_blocks/(2<<sb->s_log_block_size); ++j) {
-            printf(" %u", inode_table[i].i_block[j]);
+            printf("[%d] type: %c size: %d links: %d blocks %d\n", i + 1, file_type, inode_table[i].i_size, inode_table[i].i_links_count, inode_table[i].i_blocks);
+            printf("[%d] Blocks:", i + 1);
+            for (int j = 0; j < inode_table[i].i_blocks/(2<<sb->s_log_block_size); ++j) {
+                printf(" %u", inode_table[i].i_block[j]);
+            }
+            printf("\n");
         }
-        printf("\n");
+    }
+
+    // print directory stuff
+    printf("Directory Blocks:\n");
+
+    // printf("   DIR BLOCK NUM: %d (for inode %d)", );
+
+    // print out the root stuff first 
+    for (int j = 0; j < root.i_blocks/(2<<sb->s_log_block_size); ++j) {
+        printf("   DIR BLOCK NUM: %d (for inode %d)\n", root.i_block[j], 2);
+        
+        int start = 0;
+        while (start < EXT2_BLOCK_SIZE) {
+            struct ext2_dir_entry *dir_entry = (struct ext2_dir_entry*) (disk + EXT2_BLOCK_SIZE*root.i_block[j] + start);
+
+
+            char type;
+            if (dir_entry->file_type == EXT2_FT_DIR) {
+                type = 'd';
+            } 
+            else if (dir_entry->file_type == EXT2_FT_REG_FILE) {
+                type = 'f';
+            } 
+            else {
+                type = '0';
+            }
+
+            printf("Inode: %d rec_len: %d name_len: %d type= %c name=%.*s\n", dir_entry->inode, dir_entry->rec_len, dir_entry->name_len, type, dir_entry->name_len, dir_entry->name);
+            start += dir_entry->rec_len;
+        }
     }
 
 
+    // loop through all the inodes
+    for (int i = EXT2_GOOD_OLD_FIRST_INO; i < sb->s_inodes_count; ++i) {
+        // make sure the inode is in use and is a dir
+        if (inode_bitmap[i/8] & (1 << i % 8) && get_inode_type(inode_table, i) == 'd') {
+            // loop over the data blocks that belong to the inode
+            for (int j = 0; j < inode_table[i].i_blocks/(2<<sb->s_log_block_size); ++j) {
+                printf("   DIR BLOCK NUM: %d (for inode %d)\n", inode_table[i].i_block[j], i + 1);
+                
+                int start = 0;
+                while (start < EXT2_BLOCK_SIZE) {
+                    struct ext2_dir_entry *dir_entry = (struct ext2_dir_entry*) (disk + EXT2_BLOCK_SIZE*inode_table[i].i_block[j] + start);
+
+                    char type;
+                    if (dir_entry->file_type == EXT2_FT_DIR) {
+                        type = 'd';
+                    } 
+                    else if (dir_entry->file_type == EXT2_FT_REG_FILE) {
+                        type = 'f';
+                    } 
+                    else {
+                        type = '0';
+                    }
+
+                    printf("Inode: %d rec_len: %d name_len: %d type= %c name=%.*s\n", dir_entry->inode, dir_entry->rec_len, dir_entry->name_len, type, dir_entry->name_len, dir_entry->name);
+                    start += dir_entry->rec_len;
+                }
+            }
+    }
+    }
     return 0;
 }
